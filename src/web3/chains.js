6db72f1213c5ef5fb39faf21ff707bc09ef0d82e
David Colon <dcolonantonio@gmail.com>
2021-03-17 12:05:51 -0400

    Initial Commit - added tailwind - added web3 component and library - set up initial env - added asssets

---
+++ src/web3/chains.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const supportedChains = () => {
+  const dev = process.env.NODE_ENV !== 'production' ? [parseInt(process.env.REACT_APP_CHAINID, 10)] : []
+  return [...dev, parseInt(process.env.REACT_APP_CHAINID, 10)]
+}
+
+const useAddresses = chainId => {
+  const [addresses, setAddresses] = useState()
+
+  useEffect(() => {
+    if (chainId === parseInt(process.env.REACT_APP_CHAINID, 10)) {
+      setAddresses({
+        sarcophagusToken: process.env.REACT_APP_SARCO_TOKEN_ADDRESS,
+        SarcoStakingProxy: process.env.REACT_APP_SARCO_CONTRACT_ADDRESS,
+        SarcoVotingRightsProxy: process.env.REACT_APP_SARCO_VOTING_RIGHTS_PROXY_ADDRESS
+      })
+    }
+  }, [chainId])
+
+  return addresses
+}
+
+export {
+  supportedChains,
+  useAddresses
+} 
