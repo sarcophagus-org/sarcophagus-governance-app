941bc1268d719f5043bddaa19556aa74d1771ca4
David Colon <dcolonantonio@gmail.com>
2021-03-17 17:07:21 -0400

    - moved keys to env

--- src/web3/fallback.js
+++ src/web3/fallback.js
@@ -16,9 +16,9 @@ const useFallbackConnect = (previousProvider) => {
     }
 
     const defaultProvider = getDefaultProvider(parseInt(process.env.REACT_APP_DEFAULT_CHAIN_ID, 10), {
-      alchemy: 'bjrgvDbGZBUJ_iZFFd1vIqUWzgrwKWeg',
-      etherscan: 'G61W8715WK65NCMJPFXU8EBQ5VDTT4KCVJ',
-      infura: '800746c8737e4f34bb772ade4532ab83',
+      alchemy: process.env.REACT_APP_ALCHEMY_KEY,
+      etherscan: process.env.REACT_APP_ALCHEMY_KEY,
+      infura: process.env.REACT_APP_ALCHEMY_KEY,
     })
     setProvider(defaultProvider)
   
