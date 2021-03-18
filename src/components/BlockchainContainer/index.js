5bddfba024e1eff04ae1e7e07bc0eaf09f70544e
David Colon <dcolonantonio@gmail.com>
2021-03-18 11:49:23 -0400

    - updated titles - updated values to use bigNumbers

--- src/components/BlockchainContainer/index.js
+++ src/components/BlockchainContainer/index.js
@@ -1,3 +1,4 @@
+import { utils } from 'ethers'
 import React from 'react'
 import { useData } from '../../context/blockchain'
 import Heading from '../layout/Heading'
@@ -11,19 +12,19 @@ const BlockchainContainer = () => {
   return (
     <Section addClassnames="mt-8 py-8 flex">
       <ColumnContainer>
-        <Heading varient="heading-two" label="Your SARCO balance" />
-        <Value>{ balance } </Value>
+        <Heading varient="heading-two" label="Mys SARCO balance" />
+        <Value>{ utils.formatEther(balance) } </Value>
       </ColumnContainer>
       <ColumnContainer>
-        <Heading varient="heading-two" label="Total SARCO supply" />
-        <Value>{ totalSupply } </Value>
+        <Heading varient="heading-two" label="Total Voting Rights" />
+        <Value>{ utils.formatEther(totalSupply) } </Value>
       </ColumnContainer>
       <ColumnContainer>
-        <Heading varient="heading-two" label="Your staked SARCO" />
-        <Value>{ vrBalance } </Value>
+        <Heading varient="heading-two" label="My Voting Rights" />
+        <Value>{ utils.formatEther(vrBalance) } </Value>
       </ColumnContainer>
       <ColumnContainer>
-        <Heading varient="heading-two" label="Your voting rights" />
+        <Heading varient="heading-two" label="My Voting Power" />
         <Value>{ votingRights } </Value>
       </ColumnContainer>
     </Section>
