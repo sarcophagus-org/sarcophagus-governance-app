160291fcbf8f9ee88a15a3a99210c5894e43fed5
David Colon <dcolonantonio@gmail.com>
2021-03-17 21:04:03 -0400

    updated component with data from block chain

--- src/components/BlockchainContainer/index.js
+++ src/components/BlockchainContainer/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useData } from '../../context/blockchain'
+import Heading from '../layout/Heading'
+import Section from '../layout/Section'
+
+const ColumnContainer = ({ children }) => <div className="flex flex-col mx-8">{ children }</div>
+const Value = ({children}) => <div className="">{ children }</div>
 
 const BlockchainContainer = () => {
+  const { balance, vrBalance, totalSupply, votingRights} = useData()
   return (
-    <div>
-
-    </div>
+    <Section addClassnames="mt-8 py-8 flex">
+      <ColumnContainer>
+        <Heading varient="heading-two" label="Your SARCO balance" />
+        <Value>{ balance } </Value>
+      </ColumnContainer>
+      <ColumnContainer>
+        <Heading varient="heading-two" label="Total SARCO supply" />
+        <Value>{ totalSupply } </Value>
+      </ColumnContainer>
+      <ColumnContainer>
+        <Heading varient="heading-two" label="Your Voting Rights balance" />
+        <Value>{ vrBalance } </Value>
+      </ColumnContainer>
+      <ColumnContainer>
+        <Heading varient="heading-two" label="Your Voting Rights" />
+        <Value>{ votingRights } </Value>
+      </ColumnContainer>
+    </Section>
   )
 }
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useWeb3 } from '../web3'
 import { useAddresses } from '../web3/chains'
-// import BlockchainContainer from './BlockchainContainer'
+import BlockchainContainer from './BlockchainContainer'
 import Heading from './layout/Heading'
 
 const Link = () => {
@@ -9,15 +9,15 @@ const Link = () => {
   const addresses = useAddresses(chainId)
   const network = parseInt(chainId, 10) === 4 ? "rinkeby." : ""
   const etherscanURL = `https://${network}etherscan.io/address/${addresses?.SarcoStakingProxy}`
-  return <a href={etherscanURL} target="_blank" className="underline text-gray-400">View on etherscan</a>
+  return <a href={etherscanURL} target="_blank" rel="noreferrer noopener" className="underline text-gray-400">View on etherscan</a>
 }
 
 const Body = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center" style={{height: 'calc(100vh - 16rem'}}>
       <Heading varient="heading-one" label="SARCO Token Governence" />
       <Link />
-      {/* <BlockchainContainer /> */}
+      <BlockchainContainer />
       {/* <StakingContainer /> */}
     </div>
   )
