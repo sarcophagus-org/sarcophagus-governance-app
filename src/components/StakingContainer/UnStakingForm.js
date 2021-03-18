b23bc4bb02c3d39c2c728cc4c0255487fe5c5991
David Colon <dcolonantonio@gmail.com>
2021-03-18 02:38:52 -0400

    changed balance value to staked value for unstaking form

--- src/components/StakingContainer/UnStakingForm.js
+++ src/components/StakingContainer/UnStakingForm.js
@@ -4,13 +4,15 @@ import lock from '../../assets/images/lock.svg'
 import Input from '../forms/Input'
 import { Button } from '../forms/Button'
 import useStaking from '../hooks/useStaking'
+import { useData } from '../../context/blockchain'
 
 const UnStakingForm = () => {
-  const { balance, buttonText, buttonEnabled, calls, sarco, setSarco } = useStaking("Unstake")
+  const { buttonText, buttonEnabled, calls, sarco, setSarco } = useStaking("Unstake")
+  const { vrBalance } = useData()
   return (
     <form onSubmit={calls}>
         <div className="mt-2 flex flex-col">
-          <Input currency="sarco" value={sarco} setValue={setSarco} balance={balance} decimals={18} icon={logo} />
+          <Input currency="sarco" value={sarco} setValue={setSarco} balance={vrBalance} decimals={18} icon={logo} />
         </div>
         <div className="mt-4">
           <Button type="submit" disabled={!buttonEnabled} icon={lock}>
