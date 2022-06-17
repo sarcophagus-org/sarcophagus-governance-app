323805fa11a0d3fb4c2a41e76d31f474159f1e0d
David Colon <dcolonantonio@gmail.com>
2022-06-17 17:38:11 -0400

    updates chains to old chain.js file

--- src/context/web3Data/chains.js
+++ src/context/web3Data/chains.js
@@ -1,11 +1,4 @@
 export const supportedChains = () => {
-  const dev =
-    process.env.NODE_ENV !== 'production'
-      ? [parseInt(process.env.REACT_APP_CHAIN_ID || '0')]
-      : [];
-  const supported = [
-    ...dev,
-    ...(process.env.REACT_APP_SUPPORTED_CHAIN_IDS || '').split(',').map(i => parseInt(i)),
-  ];
-  return supported;
-};
+  const dev = process.env.NODE_ENV !== 'production' ? [parseInt(process.env.REACT_APP_CHAIN_ID, 10)] : []
+  return [...dev, parseInt(process.env.REACT_APP_CHAIN_ID, 10)]
+}
